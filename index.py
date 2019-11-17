5df9fef0cfa91f3e9ac1ade666cada8386c01305
nyq <johnstuart666@yahoo.com>
2019-11-17 15:28:23 +0800

    impliment a modification

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,request,escape
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+@app.route('/a')
+def hi():
+    name = request.args.get('name','world')
+    return f'hello,{escape(name)}!'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0',port=5000)
 
 
